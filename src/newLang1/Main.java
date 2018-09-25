181ed4224311d8e1823af1c3cf8e40a62fc0ddea
Yu Ishiguro <yuuis.egr@gmail.com>
2018-09-25 16:16:50 +0900

    add try catch and add comment

--- src/newLang1/Main.java
+++ src/newLang1/Main.java
@@ -8,19 +8,32 @@ import java.io.FileReader;
 public class Main {
     public static void main(String[] args) {
         final String SORCE_PATH = "test1.bas";
-        try {
+        File file;
+        Reader fileReader;
 
-            File file = new File(SORCE_PATH);
-            Reader fileReader = new FileReader(file);
+        // read file
+        try {
+            file = new File(SORCE_PATH);
+            fileReader = new FileReader(file);
+        } catch(IOException e) {
+            System.out.println(SORCE_PATH + " is not found.");
+            return;
+        }
+
+        // read char and print it until eof
+        try {
             for(int i = 0; i < file.length(); i++) {
                 System.out.print((char)fileReader.read());
             }
-//            while (fileReader.read() != -1) {
-//                System.out.print((char)fileReader.read());
-//            }
+        } catch (IOException e) {
+            System.out.println("io error occured.");
+        }
 
-        } catch(IOException e) {
-            System.out.println(SORCE_PATH + " is not found.");
+        // close file
+        try {
+            fileReader.close();
+        } catch (IOException e) {
+            System.out.println("file cant close.");
         }
     }
 }
