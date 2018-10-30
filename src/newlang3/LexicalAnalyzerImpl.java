a8339120ab29de3451c32b6f7f4289053ac8b81d
Yu Ishiguro <yuuis.egr@gmail.com>
2018-10-30 17:38:55 +0900

    add Map, and methods of return some lexical types

--- src/newlang3/LexicalAnalyzerImpl.java
+++ src/newlang3/LexicalAnalyzerImpl.java
@@ -1,15 +1,19 @@
 package newlang3;
 
 import java.io.*;
+import java.util.HashMap;
 
 public class LexicalAnalyzerImpl implements LexicalAnalyzer {
     PushbackReader reader;
+    static HashMap<String, LexicalType> stringMap = new HashMap<>();
+    static HashMap<String, LexicalType> symbolMap = new HashMap<>();
+    static HashMap<String, LexicalType> firstSymbolMap = new HashMap<>();
 
-    public LexicalAnalyzerImpl(String filePath) throws Exception {
-        reader = new PushbackReader(new FileReader(new File(filePath)));
+    public LexicalAnalyzerImpl(PushbackReader reader) throws Exception {
+        this.reader = reader;
     }
 
-    public LexicalUnit get() throws Exception{
+    public LexicalUnit get() throws Exception {
         while(true) {
             int ci = reader.read();
             if(ci < 0) {
@@ -17,7 +21,7 @@ public class LexicalAnalyzerImpl implements LexicalAnalyzer {
             } else {
                 char c = (char) ci;
 
-                if((c == ' ') || (c == '\t') || (c == '\r')) { continue; }
+                if((c == ' ') || (c == '\t') || (c == '\n')) { continue; }
 
                 // when alphabet
                 if((c >= 'a' && c<= 'z') || (c >= 'A' && c <= 'Z')) {
@@ -26,10 +30,21 @@ public class LexicalAnalyzerImpl implements LexicalAnalyzer {
                 }
 
                 // when number
-                if(c >= 1 && c <= 9) {
+                if(c >= '0' && c <= '9') {
                     reader.unread(ci);
                     return getInt();
                 }
+
+                // when literal
+                if(c == '"') {
+                    return getLiteral();
+                }
+
+                // when symbol
+                if(firstSymbolMap.containsKey(c + "")) {
+                    return getSymbol(c);
+                }
+
             }
         }
     }
@@ -37,7 +52,6 @@ public class LexicalAnalyzerImpl implements LexicalAnalyzer {
     private LexicalUnit getString() throws Exception {
         String target = "";
 
-        // make target
         while(true) {
             int ci = reader.read();
             char c = (char) ci;
@@ -48,12 +62,13 @@ public class LexicalAnalyzerImpl implements LexicalAnalyzer {
             reader.unread(ci);
             break;
         }
-
-        // return LexicalUnit
-        // LexicalUnit constructor receive LexicalType
-        // Todo: LexicalType contains not preserved words, so make Map
-        return new LexicalUnit(LexicalType.valueOf(target), new ValueImpl(target));
+        if(stringMap.containsKey(target)) {
+            return new LexicalUnit(stringMap.get(target));
+        } else {
+            return new LexicalUnit(LexicalType.NAME, new ValueImpl(target));
+        }
     }
+
 
     private LexicalUnit getInt() throws Exception {
         String target = "";
@@ -61,14 +76,114 @@ public class LexicalAnalyzerImpl implements LexicalAnalyzer {
         while(true) {
             int ci = reader.read();
             char c = (char) ci;
-            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+            if (c >= '0' && c <= '9') {
                 target += c;
                 continue;
             }
             reader.unread(ci);
             break;
         }
-        return new LexicalUnit(LexicalType.valueOf(target), new ValueImpl(Integer.parseInt(target)));
+        return new LexicalUnit(LexicalType.INTVAL, new ValueImpl(Integer.parseInt(target)));
+    }
+
+    private LexicalUnit getSymbol(char c) throws Exception {
+        String target = c + "";
+
+        int ci = reader.read();
+        char nextC = (char) ci;
+
+        switch (c) {
+            case '<':
+                if (nextC == '=' || nextC == '>') {
+                    target += nextC;
+                    return new LexicalUnit(symbolMap.get(target));
+                } else {
+                    reader.unread(ci);
+                    return new LexicalUnit(symbolMap.get(target));
+                }
+            case '>':
+                if (nextC == '=') {
+                    target += nextC;
+                    return new LexicalUnit(symbolMap.get(target));
+                } else {
+                    reader.unread(ci);
+                    return new LexicalUnit(symbolMap.get(target));
+                }
+            case '=':
+                if (nextC == '<' || nextC == '>') {
+                    target += nextC;
+                    return new LexicalUnit(symbolMap.get(target));
+                } else {
+                    reader.unread(ci);
+                    return new LexicalUnit(symbolMap.get(target));
+                }
+            default:
+                reader.unread(ci);
+                return new LexicalUnit(symbolMap.get(target));
+        }
+    }
+
+    private LexicalUnit getLiteral() throws Exception {
+        String target = "";
+
+        while(true) {
+            int ci = reader.read();
+            char c = (char) ci;
+            if(c != '"') {
+                target += c;
+                continue;
+            }
+            return new LexicalUnit(LexicalType.LITERAL, new ValueImpl(target));
+        }
+    }
+
+    static {
+        stringMap.put("IF", LexicalType.IF);
+        stringMap.put("THEN", LexicalType.THEN);
+        stringMap.put("ELSE", LexicalType.ELSE);
+        stringMap.put("ELSEIF", LexicalType.ELSEIF);
+        stringMap.put("FOR", LexicalType.FOR);
+        stringMap.put("FORALL", LexicalType.FORALL);
+        stringMap.put("NEXT", LexicalType.NEXT);
+        stringMap.put("SUB", LexicalType.FUNC);
+        stringMap.put("DIM", LexicalType.DIM);
+        stringMap.put("AS", LexicalType.AS);
+        stringMap.put("END", LexicalType.END);
+        stringMap.put("WHILE", LexicalType.WHILE);
+        stringMap.put("DO", LexicalType.DO);
+        stringMap.put("UNTIL", LexicalType.UNTIL);
+        stringMap.put("LOOP", LexicalType.LOOP);
+        stringMap.put("TO", LexicalType.TO);
+        stringMap.put("WEND", LexicalType.WEND);
+        firstSymbolMap.put("=", null);
+        firstSymbolMap.put("<", null);
+        firstSymbolMap.put(">", null);
+        firstSymbolMap.put(".", LexicalType.DOT);
+        firstSymbolMap.put("+", LexicalType.ADD);
+        firstSymbolMap.put("-", LexicalType.SUB);
+        firstSymbolMap.put("*", LexicalType.MUL);
+        firstSymbolMap.put("/", LexicalType.DIV);
+        firstSymbolMap.put(")", LexicalType.LP);
+        firstSymbolMap.put("(", LexicalType.RP);
+        firstSymbolMap.put(",", LexicalType.COMMA);
+        firstSymbolMap.put("\n", LexicalType.NL);
+        symbolMap.put("=", LexicalType.EQ);
+        symbolMap.put("<", LexicalType.LT);
+        symbolMap.put(">", LexicalType.GT);
+        symbolMap.put("<=", LexicalType.LE);
+        symbolMap.put("=<", LexicalType.LE);
+        symbolMap.put(">=", LexicalType.GE);
+        symbolMap.put("=>", LexicalType.GE);
+        symbolMap.put("<>", LexicalType.NE);
+        symbolMap.put(".", LexicalType.DOT);
+        symbolMap.put("+", LexicalType.ADD);
+        symbolMap.put("-", LexicalType.SUB);
+        symbolMap.put("*", LexicalType.MUL);
+        symbolMap.put("/", LexicalType.DIV);
+        symbolMap.put(")", LexicalType.LP);
+        symbolMap.put("(", LexicalType.RP);
+        symbolMap.put(",", LexicalType.COMMA);
+        symbolMap.put("\n", LexicalType.NL);
     }
 
     public boolean expect(LexicalType type) {
