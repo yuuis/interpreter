bea17f70a01b44f313a0fffddfabbedd6bee4c46
Yu Ishiguro <yuuis.egr@gmail.com>
2018-10-30 17:40:20 +0900

    move reader into main cause of file close

--- src/newlang3/Main.java
+++ src/newlang3/Main.java
@@ -1,21 +1,31 @@
 package newlang3;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PushbackReader;
+
 public class Main {
     public static void main(String[] args) {
         final String SORCE_PATH = "/Users/yuuis/tut/after_semester_3/practical_programming/interpreter/test1.bas";
+        FileReader fileReader;
 
         try {
             LexicalUnit lexicalUnit;
-            LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzerImpl(SORCE_PATH);
+            fileReader = new FileReader(new File(SORCE_PATH));
+            PushbackReader reader  = new PushbackReader(fileReader);
+            LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzerImpl(reader);
             while(true) {
                 lexicalUnit = lexicalAnalyzer.get();
+                System.out.println(lexicalUnit);
                 if(lexicalUnit.getType() == LexicalType.EOF) {
                     break;
                 }
-                System.out.println(lexicalUnit);
             }
         } catch (Exception e) {
             System.out.println(e);
+            // TODO: close of file
+//            fileReader.close();
         }
     }
 }
