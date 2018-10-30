b9b64340bd6d7439d2615400e698863bcac2a0e0
Yu Ishiguro <yuuis.egr@gmail.com>
2018-10-30 20:20:43 +0900

    move main

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,40 @@
-package newlang3;
+import newlang3.LexicalAnalyzer;
+import newlang3.LexicalAnalyzerImpl;
+import newlang3.LexicalType;
+import newlang3.LexicalUnit;
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.PushbackReader;
 
 public class Main {
     public static void main(String[] args) {
         final String SORCE_PATH = "test1.bas";
-        FileReader fileReader;
-
         try {
-            LexicalUnit lexicalUnit;
+            FileReader fileReader;
             fileReader = new FileReader(new File(SORCE_PATH));
-            PushbackReader reader  = new PushbackReader(fileReader);
-            LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzerImpl(reader);
-            while(true) {
-                lexicalUnit = lexicalAnalyzer.get();
-                System.out.println(lexicalUnit);
-                if(lexicalUnit.getType() == LexicalType.EOF) {
-                    break;
+            try {
+                LexicalUnit lexicalUnit;
+                PushbackReader reader  = new PushbackReader(fileReader);
+                LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzerImpl(reader);
+                while(true) {
+                    lexicalUnit = lexicalAnalyzer.get();
+                    System.out.println(lexicalUnit);
+                    if(lexicalUnit.getType() == LexicalType.EOF) {
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            } finally {
+                try {
+                    fileReader.close();
+                } catch (Exception e) {
+                    System.out.println(e);
                 }
             }
         } catch (Exception e) {
             System.out.println(e);
-            // TODO: close of file
-//            fileReader.close();
         }
     }
 }
