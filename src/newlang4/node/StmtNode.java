22995eca580fa9c935bbdb76cb27af30f969884c
Yu Ishiguro <yuuis.egr@gmail.com>
2018-11-21 00:14:59 +0900

    add stmt node

--- src/newlang4/node/StmtNode.java
+++ src/newlang4/node/StmtNode.java
@@ -1,7 +1,22 @@
 package newlang4.node;
 
+import newlang3.*;
 import newlang4.Node;
 
-public class StmtNode extends Node {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
+public class StmtNode extends Node {
+    static Set<LexicalType> first = new HashSet<LexicalType>(Arrays.asList());
+
+    public static Node getHandler(LexicalType lexicalType) {
+        switch (lexicalType) {
+
+        }
+    }
+
+    public static boolean isMatch(LexicalType type) {
+        return first.contains(type);
+    }
 }
