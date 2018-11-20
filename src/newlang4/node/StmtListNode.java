aeefe104b637e720c6a6c9e2e41f585c9ee289f4
Yu Ishiguro <yuuis.egr@gmail.com>
2018-11-21 00:14:29 +0900

    add stmtlist node

--- src/newlang4/node/StmtListNode.java
+++ src/newlang4/node/StmtListNode.java
@@ -1,4 +1,29 @@
 package newlang4.node;
 
-public class StmtListNode {
+import newlang3.*;
+import newlang4.Node;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class StmtListNode extends Node {
+
+    static Set<LexicalType> first = new HashSet<LexicalType>(Arrays.asList());
+
+    public boolean parse() throws Exception {
+        LexicalUnit lexicalUnit;
+        lexicalUnit = env.getInput().get();
+        if(StmtNode.isMatch(lexicalUnit.getType())) {
+            Node handler = StmtNode.getHandler(lexicalUnit.getType());
+            handler.parse();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isMatch(LexicalType type) {
+        return first.contains(type);
+    }
 }
