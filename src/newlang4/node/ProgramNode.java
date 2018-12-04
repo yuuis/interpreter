3e5e1ff1f21ecd34ef579ecd50c13ca059163948
Yu Ishiguro <yuuis.egr@gmail.com>
2018-12-04 23:44:26 +0900

    add get Handler method to ProgramNode

--- src/newlang4/node/ProgramNode.java
+++ src/newlang4/node/ProgramNode.java
@@ -1,25 +1,36 @@
 package newlang4.node;
 
 import newlang3.*;
+import newlang4.Environment;
 import newlang4.Node;
 import newlang4.NodeType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProgramNode extends Node {
+    List<Node> child = new ArrayList<Node>();
+
+    private ProgramNode(Environment env) {
+        super(env);
+        type = NodeType.PROGRAM;
+    }
 
     public boolean parse() throws Exception {
         // nodeごとの処理
         return true;
     }
 
-    public String toString() {
-        if (type == NodeType.PROGRAM) return "PROGRAM";
-        else return "Node";
+    public static Node getHandler(LexicalUnit lexicalUnit, Environment environment) {
+        return new StmtListNode(environment);
     }
 
     public static boolean isMatch(LexicalType lexicalType) {
         return true;
     }
 
-    private static Node getHandler(LexicalUnit lexicalUnit) {
+    public String toString() {
+        if (type == NodeType.PROGRAM) return "PROGRAM";
+        else return "Node";
     }
 }
