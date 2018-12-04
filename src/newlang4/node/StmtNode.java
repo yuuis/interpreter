c4237dc56dcfc926e24dc8757f002d2a25bd6702
Yu Ishiguro <yuuis.egr@gmail.com>
2018-12-04 23:45:54 +0900

    add getHandler to StmtNode

--- src/newlang4/node/StmtNode.java
+++ src/newlang4/node/StmtNode.java
@@ -3,23 +3,30 @@ package newlang4.node;
 import newlang3.*;
 import newlang4.Environment;
 import newlang4.Node;
+import newlang4.NodeType;
+
 import java.util.*;
 
 public class StmtNode extends Node {
-    static Set<LexicalType> first = new HashSet<LexicalType>(Arrays.asList());
+    static Set<LexicalType> first = new HashSet<LexicalType>(Arrays.asList(LexicalType.NAME, LexicalType.FOR, LexicalType.END));
     List<Node> child = new ArrayList<Node>();
 
-    public StmtNode(Environment env) {
+    private StmtNode(Environment env) {
         super(env);
+        type = NodeType.STMT;
     }
 
     public static Node getHandler(LexicalType lexicalType, Environment environment) {
-        switch (lexicalType) {
-
+        if (!isMatch(lexicalType)) return null;
+        else {
+            switch (lexicalType) {
+                case NAME: return StmtNode.getHandler(lexicalType, environment);
+                case FOR: return StmtNode.getHandler(lexicalType, environment);
+                case END: return StmtNode.getHandler(lexicalType, environment);
+                default: return null;
+            }
         }
     }
 
-    public static boolean isMatch(LexicalType type) {
-        return first.contains(type);
-    }
+    public static boolean isMatch(LexicalType type) { return first.contains(type); }
 }
