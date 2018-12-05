3fb262dce3c5bb07a5972a8afb5a80464e2682a9
Yu Ishiguro <yuuis.egr@gmail.com>
2018-12-05 17:03:45 +0900

    use try-with-resource statement

--- src/Main.java
+++ src/Main.java
@@ -10,28 +10,17 @@ import java.io.PushbackReader;
 public class Main {
     public static void main(String[] args) {
         final String SOURCE_PATH = "test1.bas";
-        try {
-            FileReader fileReader = new FileReader(new File(SOURCE_PATH));
-            try {
-                LexicalUnit lexicalUnit;
-                PushbackReader reader  = new PushbackReader(fileReader);
-                LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzerImpl(reader);
-                while(true) {
-                    lexicalUnit = lexicalAnalyzer.get();
-                    System.out.println(lexicalUnit);
-                    if(lexicalUnit.getType() == LexicalType.EOF) break;
-                }
-            } catch (Exception e) {
-                System.out.println(e);
-            } finally {
-                try {
-                    fileReader.close();
-                } catch (Exception e) {
-                    System.out.println(e);
-                }
+        try(FileReader fileReader = new FileReader(new File(SOURCE_PATH))) {
+            LexicalUnit lexicalUnit;
+            PushbackReader reader  = new PushbackReader(fileReader);
+            LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzerImpl(reader);
+            while(true) {
+                lexicalUnit = lexicalAnalyzer.get();
+                System.out.println(lexicalUnit);
+                if(lexicalUnit.getType() == LexicalType.EOF) break;
             }
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
 }
