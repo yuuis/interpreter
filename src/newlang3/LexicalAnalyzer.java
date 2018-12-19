676c2dd85f594cd7c7aa596d1fe1098653ae38ec
Yu Ishiguro <yuuis.egr@gmail.com>
2018-12-19 23:42:01 +0900

    add new methods

--- src/newlang3/LexicalAnalyzer.java
+++ src/newlang3/LexicalAnalyzer.java
@@ -3,5 +3,7 @@ package newlang3;
 public interface LexicalAnalyzer {
     public LexicalUnit get() throws Exception;
     public boolean expect(LexicalType type) throws Exception;
-    public void unget(LexicalUnit token) throws Exception;    
+    public void unget(LexicalUnit token) throws Exception;
+    public LexicalUnit peep(int c) throws Exception;
+    public int getLine();
 }
