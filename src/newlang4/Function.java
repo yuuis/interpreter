59622f1685655b28e9230e8ae417ac5143b22877
Yu Ishiguro <yuuis.egr@gmail.com>
2018-12-28 14:18:18 +0900

    add Function class

--- src/newlang4/Function.java
+++ src/newlang4/Function.java
@@ -1,4 +1,10 @@
 package newlang4;
 
+import newlang3.Value;
+import newlang4.node.ExprListNode;
+
 public class Function {
+    public Value invoke(ExprListNode arguments) {
+        return null;
+    }
 }
